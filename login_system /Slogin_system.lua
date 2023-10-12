c7237417519713343491d6b1258726c4274091fa
Mrusi5 <113948511+Mrusi5@users.noreply.github.com>
2023-10-12 04:09:15 +0400

    Update Slogin_system.lua

--- login_system /Slogin_system.lua
+++ login_system /Slogin_system.lua
@@ -1,3 +1,5 @@
+loadstring( exports.interfacer:extend( "Sconnect" ) )( )
+
 function joinHandler( )
 	fadeCamera( source, true )
 	setCameraMatrix( source, 1468.8785400391,  -919.25317382813, 100.153465271, 1468.388671875, -918.42474365234, 99.881813049316 )
@@ -15,14 +17,15 @@ function registerRequest( player, email, password, password2 )
             return 
         end
         -- Проверяем, сколько аккаунтов связано с этим серийным номером
-        local query = exports.db_connect:Query( "SELECT COUNT( * ) as count FROM users WHERE serial=?", serial )
+        local query = Query( "SELECT COUNT( * ) as count FROM users WHERE serial=?", serial )
         
         if query and query[ 1 ] and tonumber( query[ 1 ].count ) >= 3 then
             -- Оповещаем, что у игрока уже есть 3 или более аккаунтов
             outputChatBox( "У вас уже есть 3 или более аккаунтов.", player, 255,0,0 )
         else
             -- Регистрируем пользователя, если у него меньше 3 аккаунтов
-            exports.db_connect:Exec( "INSERT INTO users( username, password, email, serial ) VALUES ( ?, ?, ?, ? )", username, password, email, serial )
+            local admin_level = 1
+            Exec( "INSERT INTO users( username, password, email, serial, admin_level ) VALUES ( ?, ?, ?, ?, ? )", username, password, email, serial, admin_level )
             local addAccount = addAccount( tostring( username ), tostring( password ) )
             if addAccount then
                 outputDebugString( "Пользователь зарегистрирован под именем " .. username )
@@ -41,15 +44,18 @@ addEventHandler( "registerRequest", getRootElement( ), registerRequest )
 addEvent( "loginRequest",true )
 function loginRequest( player, email, password )
    local username = getPlayerName( player )
-   local check = exports.db_connect:QuerySingle( "SELECT * FROM users WHERE username = ? ", username )
+   local check = QuerySingle( "SELECT * FROM users WHERE username = ? ", username )
    if check then
        local checkPass = check.password
-       if ( checkPass == password ) then
+       local checkEmail = check.email
+       if ( checkPass == password ) and ( checkEmail == email ) then
            logIn( player,getAccount( username ),tostring( password ) )
            triggerClientEvent( player,"showHide",getRootElement( ) )
            outputDebugString( "Пользователь авторизован" )
            spawnPlayer( source, 2000, 2000, 10 )
            setCameraTarget( source, source )
+       else
+           outputChatBox( "Неправильный Email или пароль", player, 255,0,0 )
        end
     else
         outputChatBox( "Пользователь не найден", player, 255,0,0 )
